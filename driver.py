c87595bde5270bee52a884aa0eae9a2677f2c35e
Ignacio Martin Alliati <ialliati01@qub.ac.uk>
2022-10-16 17:57:55 +0100

    backtest structure done

--- driver.py
+++ driver.py
@@ -1,37 +1,71 @@
-#start
-import time
 from panterbot.execute.trading_session import trading_session
-#from panterbot.data.fetch import get_data
+from panterbot.data.fetch import get_data
+from backtesting import Backtest, Strategy
+from backtesting.lib import crossover
+import ta
 
-def go(TSDict):
+def go(TSdict):
 
-  if is_trade(TSDict):
+  if is_trade(TSdict):
     key = input('Vas a usar dinero real? \n')
     #if (key != 'paga la panter'): return
 
-  outcome_list = trading_session(TSDict)
+  outcome_list = trading_session(TSdict)
   print('Losses: '+str(outcome_list.count('loss')))
   print('Wins: '+str(outcome_list.count('profit')))
   compound = 1
   for i in range(len(outcome_list)):
-    if (outcome_list[i] == 'loss'): compound *= (1-TSDict['margin_loss'])
-    if (outcome_list[i] == 'profit'): compound *= (1+TSDict['margin_profit'])
+    if (outcome_list[i] == 'loss'): compound *= (1-TSdict['margin_loss'])
+    if (outcome_list[i] == 'profit'): compound *= (1+TSdict['margin_profit'])
   print('Profit: '+str((compound-1)*100)+'%')
-  print(outcome_list)
   return compound
+
+
+def test(TSdict):
+  """ Function to run backtests. It will instanciate
+      a Strategy and a Backtest, it will run the Backtest
+      and return/save the results. """
+
+  if is_realtime(TSdict):
+    print('This function is for tests, not for running in real time')
+    return
+ 
+  CurrentStrategy = strategy_selector(TSdict)
+
+  df = get_data()
+
+  bt = Backtest(df,CurrentStrategy,cash=TSdict['cash'])
+  
+  output = bt.run()
+
+  bt.plot()
+
+  return
   
 
-def is_realtime(TSDict):
-  if (TSDict['MODE'] == 'TRADE') or (TSDict['MODE'] == 'SIMULATION'):
+def is_realtime(TSdict):
+  if (TSdict['MODE'] == 'TRADE') or (TSdict['MODE'] == 'SIMULATION'):
     return True
   else:
     return False
 
-def is_backtest(TSDict):
-  return not is_realtime(TSDict)
+def is_backtest(TSdict):
+  return not is_realtime(TSdict)
 
-def is_trade(TSDict):
-  if (TSDict['MODE'] == 'TRADE'):
+def is_trade(TSdict):
+  if (TSdict['MODE'] == 'TRADE'):
     return True
   else:
     return False
+
+def strategy_selector(TSdict):
+  """ Function to load the chosen strategy """
+
+  if (TSdict['strategy'] == 'basic'):
+    from panterbot.strategies.basic import seektrade as CurrentStrategy
+  
+  
+  if (TSdict['strategy'] == 'macd_ema'):
+    from panterbot.strategies.macd_ema import Strat_Macd_Ema as CurrentStrategy
+    print('Imported strat')
+    return CurrentStrategy
---
+++ strategies/macd_ema.py
@@ -0,0 +1,22 @@
+from backtesting import Strategy
+from backtesting.lib import crossover
+import pandas as pd
+import ta
+
+
+class Strat_Macd_Ema(Strategy):
+  """ Strategy Class which inherits from the class Strategy
+      as defined in the backtesting library. """
+
+  def init(self):
+    close = self.data.Close
+    self.macd        = self.I(ta.trend.macd,         pd.Series(close))
+    self.macd_signal = self.I(ta.trend.macd_signal,  pd.Series(close))
+    self.ema         = self.I(ta.trend.ema_indicator,pd.Series(close),window=100)
+
+  def next(self):
+    price = self.data.Close
+    if crossover(self.macd, self.macd_signal) and price > self.ema:
+      sl = price * 0.97
+      tp = price * 1.045
+      self.buy(sl=sl,tp=tp)
